f8a5959bd33d6947c163b4cde66a4a32b982c0f9
naru <hsik0225@gmail.com>
2020-11-09 13:42:57 +0900

    refactor : jasypt config 수정

--- BE/src/main/java/com/codesquad/airbnb/common/config/JasyptConfig.java
+++ BE/src/main/java/com/codesquad/airbnb/common/config/JasyptConfig.java
@@ -3,25 +3,27 @@ package com.codesquad.airbnb.common.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.jasypt.salt.StringFixedSaltGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
 
-    String password = System.getenv("JASYPT_PASSWORD");
+    private static final String PASSWORD = System.getenv("JASYPT_PASSWORD");
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password); // 암호화에 사용할 키
-        config.setAlgorithm("PBEWithMD5AndDES"); //사용할 알고리즘
+        config.setPassword(PASSWORD);
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
-        config.setSaltGenerator(new StringFixedSaltGenerator("FixedSalt"));
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
         config.setStringOutputType("base64");
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         encryptor.setConfig(config);
         return encryptor;
     }
