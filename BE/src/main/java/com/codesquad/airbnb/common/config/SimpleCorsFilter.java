8847087f7250597e2ab6f582d6b21d866a0f8039
naru <hsik0225@gmail.com>
2020-11-09 22:17:17 +0900

    fix : cors 오류 해결 테스트

--- BE/src/main/java/com/codesquad/airbnb/common/config/SimpleCorsFilter.java
+++ BE/src/main/java/com/codesquad/airbnb/common/config/SimpleCorsFilter.java
@@ -1,13 +1,13 @@
 package com.codesquad.airbnb.common.config;
 
-import lombok.NoArgsConstructor;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@NoArgsConstructor
+import org.springframework.stereotype.Component;
+
+@Component
 public class SimpleCorsFilter implements Filter {
 
     @Override
@@ -16,21 +16,12 @@ public class SimpleCorsFilter implements Filter {
 
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods",
-                "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("Access-Control-Expose-Headers", "*, Authorization");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-                "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
         chain.doFilter(req, res);
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) {
-    }
-
-    @Override
-    public void destroy() {
     }
 }
